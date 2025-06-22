a1d7a819a06ab0d8603ac9249efa5f5108afaed5
Nilso José Baseggio Junior <nilsojr@gmail.com>
2025-06-22 13:10:58 -0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about-me', component: AboutMeComponent}
+];
